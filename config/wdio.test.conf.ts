7eaafe52134f23923b24ff9a1b6b623f45f36ef8
arma1990 <armaankhandelwal19901@gmail.com>
2023-08-17 22:17:00 +0530

    add page objects and allure report changes

--- config/wdio.test.conf.ts
+++ config/wdio.test.conf.ts
@@ -1,6 +1,20 @@
-import { config as baseConfig } from "../wdio.conf.js"
+import { config as baseConfig } from "../wdio.conf.js";
 export const config = Object.assign(baseConfig, {
-    // All test env specific key value pairs
-    environment: "TEST",
-    sauceDemoURL: "https://www.saucedemo.com"
-})
+  // All test env specific key value pairs
+  environment: "TEST",
+  sauceDemoURL: "https://www.saucedemo.com",
+  reporters: [
+    "spec",
+    [
+      "allure",
+      {
+        outputDir: "allure-results",
+        disableWebdriverStepsReporting: true,
+        useCucumberStepReporter: true,
+        reportedEnvironmentVars: {
+          Environment: 'Test'
+        }
+      },
+    ],
+  ]
+});
--- test/features/step-definitions/webInteractions.step.ts
+++ test/features/step-definitions/webInteractions.step.ts
@@ -1,24 +1,9 @@
-import { When } from "@wdio/cucumber-framework";
-import chai from "chai";
-import logger from "../../helper/logger.js";
+import { Given, When, Then } from "@wdio/cucumber-framework";
 
-When(/^Search with (.*)$/, async function (searchItem) {
-  console.log(`>> searchItem: ${searchItem}`);
-  let searchEle = await $(`textarea[name='q']`);
-  await searchEle.setValue(searchItem);
-  await browser.keys("Enter");
-});
-
-When (/^Inventory page should list (.*)$/, async function (numOfProducts) {
-  logger.info(`${this.testid}: checking the number of products...`)
-    /** 1. Check the Number of Products */
-  console.log(`The appid: ${this.appid}`);
-  if (!numOfProducts) throw Error(`Invalid product count provided: ${numOfProducts}`);
-
-  /** 2. Verify the number of products */
-  let actualProductEleArr = await $$(`.inventory_item_name`);
-  let actualProductCount = actualProductEleArr.length;
-  chai.expect(actualProductCount).to.equal(parseInt(numOfProducts)); 
+Given(/^A webpage is opened$/, async function () {
+  await browser.url("/tables");
+  await browser.setTimeout({ implicit: 15000, pageLoad: 10000 });
+  await browser.maximizeWindow();
 });
 
 When(/^Perform web interactions$/, async function () {
@@ -30,18 +15,15 @@ When(/^Perform web interactions$/, async function () {
    *    3. Click and type
    *    4. Slow Typing
    */
-
   //   let num = 12345;
   //   let strNum = num.toString();
   //   let ele = await $(`[type=number]`);
   //   await ele.click();
-
   //   for (let i = 0; i < strNum.length; i++) {
   //     let ch = strNum.charAt(i);
   //     await browser.pause(1000);
   //     await browser.keys(ch);
   //   }
-
   /**
    * 2. Dropdown
    *    Actions:
@@ -49,18 +31,15 @@ When(/^Perform web interactions$/, async function () {
    *    2. Select by attribute, text, index
    *    3. Get a list of options
    */
-
   //   // 1. Assert default option is selected
   //   let ele = await $(`select option[selected='selected']`);
   //   let val = await ele.getText();
   //   chai.expect(val).to.equal("Please select an option");
-
   //   // 2. Select by attribute, text, index
   //   let dropdownEle = await $(`#dropdown`);
   //   await dropdownEle.selectByAttribute("value", "1"); // By attribute
   //   await dropdownEle.selectByVisibleText("Option 2"); // By text
   //   await dropdownEle.selectByIndex(1); // By index
-
   //   // 3. Get a list of options
   //   let valArr = [];
   //   let eleArr = await $$(`select option`);
@@ -70,7 +49,6 @@ When(/^Perform web interactions$/, async function () {
   //     valArr.push(val);
   //   }
   //   console.log(`The Options are: ${valArr}`);
-
   /**
    * 3. Checkbox
    *    Actions:
@@ -79,14 +57,12 @@ When(/^Perform web interactions$/, async function () {
    *    3. Assert if option is selected
    *    4. Select all options
    */
-
   // let ele = $(`//form[@id='checkboxes']/input[1]`)
   // let isChecked = await ele.isSelected()
   // if (!isChecked) {
   //   await ele.click()
   //   chai.expect(isChecked).to.be.true
   // }
-
   /**
    * Web Table:
    * What are going to cover:
@@ -96,16 +72,13 @@ When(/^Perform web interactions$/, async function () {
    * 4. Get single column
    * 5. Get single cell vale (based on another cell)
    */
-
   /** 1. Check number of rows and columns */
   // let rowCount = await $$(`//table[@id='table1']/tbody/tr`).length;
   // chai.expect(rowCount).to.equal(4);
   // console.log(`Table consists rows: ${rowCount}`);
-
   // let colCount = await $$(`//table[@id='table1']/thead/tr/th`).length;
   // chai.expect(colCount).to.equal(6);
   // console.log(`Table consists rows: ${colCount}`);
-
   /** 2. Get whole table data */
   // let tableDataArr = []
   // for (let i = 0; i < rowCount; i++) {
@@ -127,7 +100,6 @@ When(/^Perform web interactions$/, async function () {
   //   tableDataArr.push(tableObj)
   // }
   // console.log(`The Table Data: ${JSON.stringify(tableDataArr)}`)
-
   /** 3. Get single row (based on the condition) */
   // let tableDataArr = []
   // for (let i = 0; i < rowCount; i++) {
@@ -149,13 +121,11 @@ When(/^Perform web interactions$/, async function () {
   //       if (j===4) tableObj.webSite = cellValue
   //     }
   //   }
-
   //   if (tableObj.firstName) {
   //     tableDataArr.push(tableObj)
   //   }
   // }
   // console.log(`The Table Data: ${JSON.stringify(tableDataArr)}`)
-
   /** 4. Get single column */
   // let tableDataArr = []
   // for (let i = 0; i < rowCount; i++) {
@@ -163,7 +133,6 @@ When(/^Perform web interactions$/, async function () {
   //   tableDataArr.push(dueCellValue)
   // }
   // console.log(`Single Cell Value: ${tableDataArr}`)
-
   /** 5. Get single cell vale (based on another cell) */
   // let tableDataArr = [];
   // for (let i = 0; i < rowCount; i++) {
@@ -175,21 +144,17 @@ When(/^Perform web interactions$/, async function () {
   //   }
   // }
   // console.log(`Single Cell Value: ${JSON.stringify(tableDataArr)}`);
-
   /** Scrolling
-   *  
+   *
    *  VISIBLE PORTION
    *  Windows Object
    *  1. ScrollBy
    *  Y -> [-]window.innerHeight
    */
-
-  /** 
+  /**
    * INVISIBLE PORTION
    * Windows Object
    * 1. scrollTo
-   * Y -> document.body.scrollTop[scrollHeight] 
+   * Y -> document.body.scrollTop[scrollHeight]
    */
-
 });
-
